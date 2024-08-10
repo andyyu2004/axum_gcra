c5829af7013c1343a2621b7a7e9682fb4c27f2d7
novacrazy <novacrazy@gmail.com>
2024-08-10 15:20:26 -0500

    Add more ip extraction headers

--- src/real_ip.rs
+++ src/real_ip.rs
@@ -15,18 +15,20 @@ use std::{
 /// This extractor will try to get the real IP address of the client, using the following headers, in order:
 /// - `x-forwarded-for`
 /// - `x-real-ip`
+/// - `true-client-ip` (used by some load balancers)
 /// - `client-ip` (used by some load balancers)
 /// - `x-cluster-client-ip` (used by AWS sometimes)
 /// - `cf-connecting-ip` (used by Cloudflare sometimes)
+/// - `fly-client-ip` (used by Fly.io sometimes)
+/// - `cloudfront-viewer-address` (used by Cloudfront sometimes)
 ///
 /// If none of these headers are found, it will return a 400 Bad Request via [`IpAddrRejection`],
 /// or the error can be handled with a custom rejection handler with
 /// [`RateLimitLayerBuilder::handle_error`](crate::RateLimitLayerBuilder::handle_error).
 ///
-///
 /// If the `tokio` feature is enabled AND the `Router` is configured with
 /// [`Router::into_make_service_with_connect_info`](axum::Router::into_make_service_with_connect_info),
-/// it will also try to get the IP address from the underlying socket via the [`ConnectInfo`](axum::extract::ConnectInfo) extension.
+/// it will also try to get the IP address from the underlying socket via the [`ConnectInfo<SocketAddr>`](axum::extract::ConnectInfo) extension.
 /// This is optional as it may not work as expected if the server is behind a reverse proxy.
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)]
@@ -71,15 +73,20 @@ impl<S> FromRequestParts<S> for RealIp {
 
     async fn from_request_parts(parts: &mut Parts, _: &S) -> Result<Self, Self::Rejection> {
         fn parse_ip(s: &HeaderValue) -> Option<IpAddr> {
-            s.to_str().ok().and_then(|s| s.split(',').next().and_then(|s| IpAddr::from_str(s.trim()).ok()))
+            s.to_str()
+                .ok()
+                .and_then(|s| s.split(&[',', ':']).next().and_then(|s| IpAddr::from_str(s.trim()).ok()))
         }
 
-        static HEADERS: [HeaderName; 5] = [
+        static HEADERS: [HeaderName; 8] = [
             HeaderName::from_static("x-forwarded-for"),
             HeaderName::from_static("x-real-ip"),
-            HeaderName::from_static("client-ip"), // used by some load balancers
+            HeaderName::from_static("true-client-ip"), // used by some load balancers
+            HeaderName::from_static("client-ip"),      // used by some load balancers
             HeaderName::from_static("x-cluster-client-ip"), // used by AWS sometimes
             HeaderName::from_static("cf-connecting-ip"), // used by Cloudflare sometimes
+            HeaderName::from_static("fly-client-ip"),  // used by Fly.io sometimes
+            HeaderName::from_static("cloudfront-viewer-address"), // used by Cloudfront sometimes
         ];
 
         for header in &HEADERS {
