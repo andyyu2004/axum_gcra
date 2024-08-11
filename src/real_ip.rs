293334aa87bf4a2a692ce3b6529e464f6affd900
novacrazy <novacrazy@gmail.com>
2024-08-11 00:47:29 -0500

    Add RealIp layer

--- src/real_ip.rs
+++ src/real_ip.rs
@@ -1,14 +1,21 @@
 //! Provides a way to get the real IP address of the client, to the best of our ability.
+//!
+//! This is exposed as [`RealIp`], which is an extractor that can be used in Axum routes,
+//! or as a [`Layer`] and [`Service`] that can be used to add the IP address to
+//! the request parts extensions. Or both.
 
-use axum::{extract::FromRequestParts, response::IntoResponse};
-use http::{header::HeaderName, request::Parts, HeaderValue, StatusCode};
 use std::{
     fmt::{self, Debug, Display},
     hash::Hash,
     net::{IpAddr, SocketAddr},
     ops::Deref,
     str::FromStr,
+    task::{Context, Poll},
 };
+
+use axum::{extract::FromRequestParts, response::IntoResponse};
+use http::{header::HeaderName, request::Parts, HeaderValue, Request, StatusCode};
+use tower::{Layer, Service};
 
 /// Wrapper around `std::net::IpAddr` that can be extracted from the request parts.
 ///
@@ -72,34 +79,89 @@ impl<S> FromRequestParts<S> for RealIp {
     type Rejection = IpAddrRejection;
 
     async fn from_request_parts(parts: &mut Parts, _: &S) -> Result<Self, Self::Rejection> {
-        fn parse_ip(s: &HeaderValue) -> Option<IpAddr> {
-            s.to_str()
-                .ok()
-                .and_then(|s| s.split(&[',', ':']).next().and_then(|s| IpAddr::from_str(s.trim()).ok()))
+        if let Some(ip) = parts.extensions.get::<RealIp>() {
+            return Ok(*ip);
         }
 
-        static HEADERS: [HeaderName; 8] = [
-            HeaderName::from_static("x-forwarded-for"),
-            HeaderName::from_static("x-real-ip"),
-            HeaderName::from_static("true-client-ip"), // used by some load balancers
-            HeaderName::from_static("client-ip"),      // used by some load balancers
-            HeaderName::from_static("x-cluster-client-ip"), // used by AWS sometimes
-            HeaderName::from_static("cf-connecting-ip"), // used by Cloudflare sometimes
-            HeaderName::from_static("fly-client-ip"),  // used by Fly.io sometimes
-            HeaderName::from_static("cloudfront-viewer-address"), // used by Cloudfront sometimes
-        ];
-
-        for header in &HEADERS {
-            if let Some(real_ip) = parts.headers.get(header).and_then(parse_ip) {
-                return Ok(RealIp(real_ip));
-            }
+        match get_ip_from_parts(parts) {
+            Some(ip) => Ok(RealIp(ip)),
+            None => Err(IpAddrRejection),
         }
-
-        #[cfg(feature = "tokio")]
-        if let Some(info) = parts.extensions.get::<axum::extract::ConnectInfo<SocketAddr>>() {
-            return Ok(RealIp(info.ip()));
-        }
-
-        Err(IpAddrRejection)
     }
+}
+
+/// [`Service`] that adds the [`RealIp`] extension to the request parts if available.
+///
+/// This extension can be reused by other services or extractors, such as [`RealIp`] itself.
+#[derive(Debug, Clone, Copy)]
+pub struct RealIpService<I>(I);
+
+/// [`Layer`] that adds the [`RealIp`] extension to the request parts if available.
+///
+/// This extension can be reused by other services or extractors, such as [`RealIp`] itself.
+#[derive(Debug, Clone, Copy)]
+pub struct RealIpLayer;
+
+impl<B, I> Service<Request<B>> for RealIpService<I>
+where
+    I: Service<Request<B>>,
+{
+    type Response = I::Response;
+    type Error = I::Error;
+    type Future = I::Future;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.0.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: Request<B>) -> Self::Future {
+        let (mut parts, body) = req.into_parts();
+
+        if let Some(ip) = get_ip_from_parts(&parts) {
+            parts.extensions.insert(RealIp(ip));
+        }
+
+        self.0.call(Request::from_parts(parts, body))
+    }
+}
+
+impl<I> Layer<I> for RealIpLayer {
+    type Service = RealIpService<I>;
+
+    fn layer(&self, inner: I) -> Self::Service {
+        RealIpService(inner)
+    }
+}
+
+fn get_ip_from_parts(parts: &Parts) -> Option<IpAddr> {
+    fn parse_ip(s: &HeaderValue) -> Option<IpAddr> {
+        s.to_str()
+            .ok()
+            .and_then(|s| s.split(&[',', ':']).next())
+            .and_then(|s| IpAddr::from_str(s.trim()).ok())
+    }
+
+    static HEADERS: [HeaderName; 8] = [
+        HeaderName::from_static("x-forwarded-for"),
+        HeaderName::from_static("x-real-ip"),
+        HeaderName::from_static("true-client-ip"), // used by some load balancers
+        HeaderName::from_static("client-ip"),      // used by some load balancers
+        HeaderName::from_static("x-cluster-client-ip"), // used by AWS sometimes
+        HeaderName::from_static("cf-connecting-ip"), // used by Cloudflare sometimes
+        HeaderName::from_static("fly-client-ip"),  // used by Fly.io sometimes
+        HeaderName::from_static("cloudfront-viewer-address"), // used by Cloudfront sometimes
+    ];
+
+    for header in &HEADERS {
+        if let Some(real_ip) = parts.headers.get(header).and_then(parse_ip) {
+            return Some(real_ip);
+        }
+    }
+
+    #[cfg(feature = "tokio")]
+    if let Some(info) = parts.extensions.get::<axum::extract::ConnectInfo<SocketAddr>>() {
+        return Some(info.ip());
+    }
+
+    None
 }
